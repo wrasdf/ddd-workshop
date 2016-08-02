917353d8d6115e2a4b39a13f3a536f705090128d
Kerry.Wang <kerry.wang@myob.com>
2016-08-02 16:36:22 +1000

    fix shell variable issue

--- day-2/console.sh
+++ day-2/console.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+
 PROJECT_NAME="day-2"
-docker build -t PROJECT_NAME .
-docker run -d -p 4000:4000 -v $(pwd)/redux-todo:/root/app -v /root/app/node_modules PROJECT_NAME npm run start
+docker build -t $PROJECT_NAME .
+docker run -d -p 4000:4000 -v $(pwd)/redux-todo:/root/app -v /root/app/node_modules $PROJECT_NAME npm start
